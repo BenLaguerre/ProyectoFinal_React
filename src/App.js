8cfc01528060fd44a94644012e92b3b23481f659
Benoit Laguerre <be.laguerre@gmail.com>
2021-12-09 11:32:58 +0100

    lechones-gusatado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import LechonGustado from './components/lechones-gustados/lechones-gustados';
+import axios from "axios";
 import DetailleLechon from './components/detalle-lechon/detalle-lechon';
 
 function App() {
-  return (
-    <div className="App">
-      <DetailleLechon />
-    </div>
-  );
-}
+  
+  /*const [lechonLike, setLechonLike] = useState([]);
+  
+  useEffect(() => {
+    cargarPerfil(22);
+  }, []);
+
+
+  async function cargarPerfil(id) {
+    await axios.get(`http://localhost:8000/detalle-perfil/${id}`).then((res) => {
+      setLechonLike(res.data[0].arrayLikes);
+    })
+  }
+
+  if (lechonLike.length === 0) {
+    return null
+  } else {
+    let lechones  = lechonLike.map(item => (
+      <div>
+        <LechonGustado email = {item} />
+      </div>
+    ))*/
+
+    return (
+      <div className="App">
+         <LechonGustado />
+      </div>
+    );
+  }
+
 
 export default App;
 
--- src/components/lechones-gustados/lechones-gustados.js
+++ src/components/lechones-gustados/lechones-gustados.js
@@ -1,41 +1,44 @@
 import React, { useEffect, useState } from "react";
+import { Router } from "react-router";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
 export default function LechonGustado() {
   useEffect(() => {
-    cargarDetaille();
+    cargarPerfil(22);
   }, []);
+
+
+  const [lechonLike, setLechonLike] = useState([]);
 
   const [lechon, setLechon] = useState([]);
 
-  async function cargarDetaille(email) {
-    await axios
-      .get(`http://localhost:8000/detalle-perfil`,{email:email})
-      .then((res) => {
-        setLechon(res.data);
-      });
+
+  function cargarPerfil(id) {
+    axios.get(`http://localhost:8000/detalle-perfil/${id}`).then((res) => {
+      setLechonLike(res.data[0].arrayLikes);
+    })
   }
 
+  function cargarDetaille(email) {
+    axios
+      .get(`http://localhost:8000/detalle-perfil`,{ params: {
+        email: email
+      }})
+      .then((res) => {
+        setLechon(res.data);
+      })
+  }
 
+  
 
-  return (
-    <div class="main-container">
-      <h2>hello there</h2>
-      <div class="lechon-container">
-        <div class="img-container">
-          <img className='mat-card-image' src={lechon.image} alt='imagencita' />
-        </div>
-        <div>
-          <Link className="boton" to={`../detalle-lechon/${lechon.id}`}>
-            {lechon.name.firstName}
-          </Link>
-        </div>
-        <div>
-          <p>Ciudad:{lechon.city}</p>
-          <p>Edad: {lechon.age}</p>
-        </div>
-      </div>
-    </div>
-  );
-}
+    return (
+      <>
+      <h2>Welcome!</h2>
+      <div>{cargarDetaille(lechonLike[0])}</div>
+      </>
+    )
+  }
+  
+
+ 
