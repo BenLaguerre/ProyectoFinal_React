857fc75e15623011e17379bd966ff78e56291eec
Benoit Laguerre <be.laguerre@gmail.com>
2021-12-09 13:09:20 +0100

    actualizar DetailleLechon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import React, { useEffect, useState } from "react";
-=======
 import Header from './components/header/header';
->>>>>>> 5ae22e197c104bb2e03e11a25cd4eac49a85f07e
 import './App.css';
 import LechonGustado from './components/lechones-gustados/lechones-gustados';
 import axios from "axios";
@@ -14,38 +9,6 @@ import LechonesGustados from './components/lechones-gustados/lechones-gustados'
 
 
 function App() {
-<<<<<<< HEAD
-  
-  /*const [lechonLike, setLechonLike] = useState([]);
-  
-  useEffect(() => {
-    cargarPerfil(22);
-  }, []);
-
-
-  async function cargarPerfil(id) {
-    await axios.get(`http://localhost:8000/detalle-perfil/${id}`).then((res) => {
-      setLechonLike(res.data[0].arrayLikes);
-    })
-  }
-
-  if (lechonLike.length === 0) {
-    return null
-  } else {
-    let lechones  = lechonLike.map(item => (
-      <div>
-        <LechonGustado email = {item} />
-      </div>
-    ))*/
-
-    return (
-      <div className="App">
-         <LechonGustado />
-      </div>
-    );
-  }
-
-=======
   return (
     <>
     <Router>
@@ -57,13 +20,12 @@ function App() {
 					</Routes>
 				</main>
     <div className="App">
-    <LechonesGustados />
+    
     </div>
     </Router>
     </>
   );
 }
->>>>>>> 5ae22e197c104bb2e03e11a25cd4eac49a85f07e
 
 export default App;
 
